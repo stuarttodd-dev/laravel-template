67df018af85de75aaf05a5d1aed1f8dc6235a0c6
Stuart Todd <hello@halfshellstudios.co.uk>
2025-11-09 09:29:20 +0000

    add handleIneritaRequets into web middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,6 +34,7 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\HandleInertiaRequests::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
@@ -64,5 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'ensure_super_admin' => \App\Http\Middleware\EnsureSuperAdmin::class,
     ];
 }
